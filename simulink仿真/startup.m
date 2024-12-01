e4fe797f3d337013df44a4310b36d024d25f9af2
cdh <87925267+cdh66666@users.noreply.github.com>
2024-12-01 15:01:10 +0800

    1

---
+++ simulink仿真/startup.m
@@ -0,0 +1,22 @@
+clear;
+clc;
+
+global freq Uo
+%初始化输出频率freq,输出电压Uo
+freq = 25000;
+Uo = 12;
+
+ 
+ 
+
+% 设置要运行的 slx 文件路径
+slxFileName = 'us_power.slx'; 
+
+% 直接调用 open 函数并将输出重定向到临时变量，避免阻塞
+tmp = open(slxFileName);
+
+% 加载模型
+load_system(slxFileName);
+
+% 运行模型
+sim(slxFileName);
